3e162f9076c028640144ea37d623f29b282f85ff
Priyanka <Priyanka.mydesire@gmail.com>
2020-10-22 13:56:04 +0200

    Final commit : with the functionality of clicking the locations on map and based on the location i am showing the relevant information fetched from the tomorrow API and after categorising it into three major categories, i am showing if it lies into Excellent, Good or poor category for using resources. On the basis of how much these locations using natural resources i am showing a pie chart which shows the percentage of these natural resources usage.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Tomorrow
+
 A small app for saving Tomorrow
+
+This is a react based app which can act as a platform to make consumers aware on the type of usage of energy. How much environment friendly is the energy consumption or its source. This will give an opportunity to the consumer to move from non renewable source of electricity towards renewable source.
+The app lets the user select a particular location on the world map. The accuracy of the location increases if you zoom in. On clicking the location, the app displays the following information:
+
+-datetime
+-fossilFreePercentage
+-powerConsumptionTotal
+-renewablePercentage
+-biomass
+-coal
+-gas
+-geothermal
+-hydro
+-nuclear
+-oil
+-solar
+-Others
+-wind
+
+Based on predefined threshold, the app categorizes the consumption into a pie chart with clear division of the share each electricity generation source occupies in that region.Then we have a color indicator which is calculated internally as a mean of the fossilFreePercentage & renewablePercentage parameter values. On the scaler of 1 to 100,shades of green, amber and red depicts how eco friendly is the source of electricity generation in that area. Green indicates high alignment with our environment.
--- tomorrow/src/carbon.js
+++ tomorrow/src/carbon.js
@@ -1,33 +1,38 @@
 import React, { useState } from "react";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import MapGL, { GeolocateControl } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { PieChart } from "react-chartkick";
+import "chart.js";
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoicHJpeWFua2FwcyIsImEiOiJja2cyYzFjY3MxZTc4MnlxZm92d2Y4M3poIn0.7Eb13DlMMMXb-_UnsMgcVg";
 
-const geolocateStyle = {};
+const geolocateStyle = {
+  display: "inline-block",
+  margin: "10px",
+  padding: "5px",
+};
 
 function Carbon() {
   const [position, setPosition] = useState({
-    longitude: 48.8566,
-    latitude: 2.3522,
+    longitude: 12,
+    latitude: 55,
   });
 
   const onMapClick = (event) => {
-    const lng = event.lngLat.slice(0, 1);
-    const lat = event.lngLat.slice(1, 2);
+    const lng = parseInt(event.lngLat.slice(0, 1));
+    const lat = parseInt(event.lngLat.slice(1, 2));
     setPosition({ longitude: lng, latitude: lat });
     getPowerBreakdown();
   };
 
   const [viewport, setViewport] = useState({
     width: "100%",
-    height: 400,
+    height: 310,
     latitude: 55.676098,
     longitude: 12.568337,
-    zoom: 5,
+    zoom: 4,
     bearing: 0,
     pitch: 0,
     marker: true,
@@ -36,7 +41,26 @@ function Carbon() {
   const _onViewportChange = (viewport) =>
     setViewport({ ...viewport, transitionDuration: 3000 });
 
-  const [CountryInfo, SetCountryInfo] = useState({ zone: "" });
+  const [CountryInfo, SetCountryInfo] = useState({
+    zone: "DK-DK2",
+    fossilFreePercentage: "91",
+    renewablePercentage: "85",
+    powerConsumptionTotal: "1684",
+    powerConsumptionBreakdown: {
+      "battery discharge": 0,
+      biomass: 276,
+      coal: 61,
+      gas: 45,
+      geothermal: 0,
+      hydro: 268,
+      "hydro discharge": 5,
+      nuclear: 100,
+      oil: 14,
+      solar: 0,
+      unknown: 29,
+      wind: 886,
+    },
+  });
 
   const getPowerBreakdown = async () => {
     const apiCall = await fetch(
@@ -51,40 +75,84 @@ function Carbon() {
       }
     );
     const user = await apiCall.json();
-
     SetCountryInfo(user);
   };
 
-  if (!CountryInfo) {
-    console.log("Page is loading please wait.....");
+  // making an array for rechart here
+
+  const chartInput = {
+    Biomass: CountryInfo.powerConsumptionBreakdown.biomass,
+    coal: CountryInfo.powerConsumptionBreakdown.coal,
+    gas: CountryInfo.powerConsumptionBreakdown.gas,
+    geothermal: CountryInfo.powerConsumptionBreakdown.geothermal,
+    hydro: CountryInfo.powerConsumptionBreakdown.hydro,
+    nuclear: CountryInfo.powerConsumptionBreakdown.nuclear,
+    oil: CountryInfo.powerConsumptionBreakdown.oil,
+    solar: CountryInfo.powerConsumptionBreakdown.solar,
+    wind: CountryInfo.powerConsumptionBreakdown.wind,
+    Others: CountryInfo.powerConsumptionBreakdown.unknown,
+  };
+
+  // environment friendly ratio
+
+  const Mean =
+    (parseInt(CountryInfo.fossilFreePercentage) +
+      parseInt(CountryInfo.renewablePercentage)) /
+    2;
+
+  let GetColor = {};
+  let PresentData = {};
+
+  if (!CountryInfo.powerConsumptionTotal) {
+    PresentData = "nodata";
+  } else {
+    PresentData = "hasdata";
+  }
+
+  if (Mean > 70) {
+    GetColor = "Excellent";
+  } else if (Mean > 50) {
+    GetColor = "Good";
+  } else {
+    GetColor = "Poor";
   }
 
   return (
     <div className="container-fluid">
       <div className="container">
-        <h2>See the carbon emission </h2>
+        <h2>How Clean is Your Electricity </h2>
         <p>
-          Select a country name and see the carbon emission in that country.
+          Want to know how environment friendly is the electricity generation in
+          you area? Click on any location in the map to find out!!
         </p>
-        <div>
-          <div>
-            <MapGL
-              {...viewport}
-              mapboxApiAccessToken={MAPBOX_TOKEN}
-              mapStyle="mapbox://styles/mapbox/dark-v8"
-              onViewportChange={_onViewportChange}
-              onClick={onMapClick}
-            >
-              <GeolocateControl
-                style={geolocateStyle}
-                positionOptions={{ enableHighAccuracy: true }}
-                trackUserLocation={true}
-              />
-            </MapGL>
-            ;
-          </div>
+          <small><em>For more accuracy try zooming in.</em></small>
 
-          <div className="Datashow" id="Datashow">
+        <div className="row">
+          <MapGL
+            {...viewport}
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+            mapStyle="mapbox://styles/mapbox/dark-v8"
+            onViewportChange={_onViewportChange}
+            onClick={onMapClick}
+          >
+            <GeolocateControl
+              style={geolocateStyle}
+              positionOptions={{ enableHighAccuracy: true }}
+              trackUserLocation={true}
+            />
+          </MapGL>
+        </div>
+
+        <div className="row pt-4" id={PresentData}>
+          <div className="container nodatamsg">
+            <h2>Insufficient Data </h2>
+            <p>
+              We don't have any data related to this location at the moment but
+              we are adding more locations into our database. </p>
+            <p>  Please try another location for now, Thanks.</p>
+            
+          </div>
+          <div className="Datashow col-5">
             {CountryInfo.zone === "" ? (
               <h3> Welcome, please click on the map to see details</h3>
             ) : CountryInfo.status === "error" ? (
@@ -92,43 +160,30 @@ function Carbon() {
             ) : (
               <div>
                 <h3> Zone {CountryInfo.zone}</h3>
+                <h4>
+                  is rated <span className={GetColor}>{GetColor}</span>
+                </h4>
+
+                <p>for environment friendly electricity consumption</p>
                 <ul>
-                  <li>datetime: {CountryInfo.datetime}</li>
                   <li>
-                    fossilFreePercentage: {CountryInfo.fossilFreePercentage}
+                    Fossil Free Percentage : {CountryInfo.fossilFreePercentage}
                   </li>
                   <li>
-                    powerConsumptionTotal: {CountryInfo.powerConsumptionTotal}
+                    Renewable Percentage : {CountryInfo.renewablePercentage}
                   </li>
                   <li>
-                    renewablePercentage: {CountryInfo.renewablePercentage}
+                    Total Power Consumption :{" "}
+                    {CountryInfo.powerConsumptionTotal}
                   </li>
-                  <li>
-                    <hr />
-                  </li>
-                  <li>
-                    biomass: {CountryInfo.powerConsumptionBreakdown.biomass}
-                  </li>
-                  <li>coal: {CountryInfo.powerConsumptionBreakdown.coal}</li>
-                  <li>gas: {CountryInfo.powerConsumptionBreakdown.gas}</li>
-                  <li>
-                    geothermal:{" "}
-                    {CountryInfo.powerConsumptionBreakdown.geothermal}
-                  </li>
-                  <li>hydro: {CountryInfo.powerConsumptionBreakdown.hydro}</li>
-                  <li>
-                    nuclear: {CountryInfo.powerConsumptionBreakdown.nuclear}
-                  </li>
-                  <li>oil: {CountryInfo.powerConsumptionBreakdown.oil}</li>
-                  <li>solar: {CountryInfo.powerConsumptionBreakdown.solar}</li>
-                  <li>
-                    Others: {CountryInfo.powerConsumptionBreakdown.unknown}
-                  </li>
-                  <li>wind: {CountryInfo.powerConsumptionBreakdown.wind}</li>
                 </ul>
               </div>
             )}
             ​
+          </div>
+
+          <div className="piechart col-6">
+            <PieChart data={chartInput} />
           </div>
         </div>
       </div>
